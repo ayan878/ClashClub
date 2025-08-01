d0f0e11782ed1229443b540b9286c9d376f298fe
ayan878 <ayanraza197@gmail.com>
2025-08-01 16:32:39 +0530

    socket fix

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -10,13 +10,13 @@
 import { io } from "socket.io-client";
 
 // Detect environment and set correct URL
-const URL =
-  // eslint-disable-next-line no-undef
-  process.env.NODE_ENV === "production"
-    ? "https://clash-club-backend.vercel.app"
-    : "http://localhost:3000";
+// const URL =
+//   // eslint-disable-next-line no-undef
+//   process.env.NODE_ENV === "production"
+//     ? "https://clash-club-backend.vercel.app"
+//     : "http://localhost:3000";
 
-export const socket = io(URL);
+export const socket = io("https://clash-club-backend.vercel.app");
 // export const socket = io(URL, {
 //   withCredentials: true, // Optional: for cookies/auth if used
 // });
